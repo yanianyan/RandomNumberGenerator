df19e3a77a9463b63c2e8e5cfa7963d9286ecc32
Yanyan <146277878+yanianyan@users.noreply.github.com>
2023-11-01 23:29:36 +0800

    Initial commit

---
+++ randomNumber.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+
+win = Tk()
+win.title("Random Number Generator")
+win.geometry("550x450")
+
+
+#GUI
+
+title_label = Label(win, text="Random Number Generator", font=("Comic Sans MS", 20, "bold"))
+title_label.pack()
+win.mainloop()
