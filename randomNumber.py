52c01c51516cb09640fbc4512fdadcb68924c7d6
Yanyan <146277878+yanianyan@users.noreply.github.com>
2023-11-03 16:07:11 +0800

    added some functions

--- randomNumber.py
+++ randomNumber.py
@@ -2,12 +2,26 @@ from tkinter import *
 import random
 
 win = Tk()
-win.title("Random Number Generator")
+win.title("Random Number Generator 1-100")
 win.geometry("550x450")
+win.grid()
 
+def randomnum(event):
+    value=random.randint(1,101)
+    print(value)
+    updateDisplay(value)
 
+def updateDisplay(myString):
+    displayVariable.set(myString)
 #GUI
 
 title_label = Label(win, text="Random Number Generator", font=("Comic Sans MS", 20, "bold"))
 title_label.pack()
+button_1 =Button(win, text="Click Here")
+button_1.bind("<1>",randomnum)
+button_1.pack()
+displayVariable = StringVar()
+Label1 = Label(win, textvariable = displayVariable, font=("Comic Sans MS", 20, "bold"))
+Label1.pack(side=TOP, padx=30, pady=30)
+
 win.mainloop()
